13bb618b71bea8870b360dafed26a95ac468a74b
islam <eessllaamm96@gmail.com>
2023-02-17 21:45:53 +0200

    first

---
+++ users/index.php
@@ -0,0 +1,45 @@
+<?php
+// $user="";
+// echo isset($usert);
+require_once("../connection.php");
+// $action=isset($_GET["action"])?$_GET["action"]:"";
+
+// get all data from users
+
+$sql="select * from users";
+$stmt=$db->prepare($sql);
+$stmt->execute();
+$result=$stmt->fetchAll(pdo :: FETCH_ASSOC);
+
+// get all data from phones
+$sql="select * from phones";
+$stmt=$db->prepare($sql);
+$stmt->execute();
+$phone=$stmt->fetchAll(pdo :: FETCH_ASSOC);
+
+// get all data from items
+$sql="select * from items";
+$stmt=$db->prepare($sql);
+$stmt->execute();
+$item=$stmt->fetchAll(pdo :: FETCH_ASSOC);
+// print_r($item);
+// foreach ($item as $value) {
+//     echo "$value[item_id]
+//     $value[item_status]
+//     ";
+// }
+foreach ($item as $value) {
+    if ("$value[item_status]" == "pending") {
+        $pending[] = $value;
+        // print_r($pending);
+    }elseif ("$value[item_status]" == "approved") {
+        $approved[] = $value;
+        // print_r($approved);
+    }elseif ("$value[item_status]" == "rejected") {
+        $rejected[] = $value;
+        // print_r($rejected);
+    }
+}
+
+// inject view ---------
+require("views/user.php");
